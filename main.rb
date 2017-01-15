67473d7ae166cc9ce2f8dd2df80809fd0486039b
howaiyung413 <howaiyung1988@gmail.com>
2017-01-14 22:59:47 -0700

    Universal object

--- main.rb
+++ main.rb
@@ -2,10 +2,27 @@
 
 puts "Hello, Ruby!";
 
-class Order
+def check_int(var, num)
+  raise ArgumentError, '#var is not an integer' unless num.is_a? Integer
+end
+class object
   @@total_clients=0
   @@vegetarian=0
   @@glutenFree=0
   @@nutFree=0
   @@fishFree=0
+
+  def initializer(t_c, veg, g_f, n_f, f_f)
+
+    check_int('Total number of clients ', t_c)
+    check_int('Number of vegetarians ', veg)
+
+    @@total_clients=t_c
+    @@vegetarian=veg
+    @@glutenFree=g_f
+    @@nutFree=n_f
+    @@fishFree=f_f
+  end
 end
+
+restaurant_1=object(4,5,6,7,8)
