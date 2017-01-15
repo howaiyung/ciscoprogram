18903f2de4b2fe83741d95d6113b216e71efd3ad
howaiyung413 <howaiyung1988@gmail.com>
2017-01-15 00:53:17 -0700

    Spaghetti code on user input plus additional notes

--- main.rb
+++ main.rb
@@ -1,11 +1,30 @@
 #C:\Ruby22-x64\bin -w
 
-puts "Hello, Ruby!";
 
-def check_int(var, num)
-  raise ArgumentError, '#var is not an integer' unless num.is_a? Integer
+
+
+
+class Team_Order
+  @@total_clients=0
+  @@vegetarian=0
+  @@glutenFree=0
+  @@nutFree=0
+  @@fishFree=0
+  @@allergyFree=0
+
+  def initializer(t_c, veg, g_f, n_f, f_f)
+
+    @@total_clients=t_c
+    @@vegetarian=veg
+    @@glutenFree=g_f
+    @@nutFree=n_f
+    @@fishFree=f_f
+    @@allergyFree=t_c-(veg+g_f+n_f+f_f)
+
+  end
 end
-class object
+
+class Rest_Stats
   @@total_clients=0
   @@vegetarian=0
   @@glutenFree=0
@@ -13,10 +32,6 @@ class object
   @@fishFree=0
 
   def initializer(t_c, veg, g_f, n_f, f_f)
-
-    check_int('Total number of clients ', t_c)
-    check_int('Number of vegetarians ', veg)
-
     @@total_clients=t_c
     @@vegetarian=veg
     @@glutenFree=g_f
@@ -25,4 +40,71 @@ class object
   end
 end
 
-restaurant_1=object(4,5,6,7,8)
+
+class Main
+
+  @@t_c=0
+  @@veg=0
+  @@g_f=0
+  @@n_f=0
+  @@f_f=0
+
+  puts "Welcome to my restaurant customizer"
+
+  # Create a function for this input
+  # Create another function for error checking ints
+
+  puts "How many meals are we ordering for the team?: "
+  @@t_c = Integer(gets) rescue nil
+
+  until @@t_c != nil do
+    puts "Invalid input, please retype the number of meals been ordered: "
+    @@t_c = Integer(gets) rescue nil
+  end
+
+  puts "How many meals of those meals are vegetarian?: "
+  @@veg = Integer(gets) rescue nil
+
+  until @@veg != nil do
+    puts "Invalid input, please retype the number of vegetarian meals been ordered: "
+    @@veg = Integer(gets) rescue nil
+  end
+
+  puts "How many meals of those meals are gluten free?: "
+  @@g_f = Integer(gets) rescue nil
+
+  until @@g_f != nil do
+    puts "Invalid input, please retype the number of gluten free meals been ordered: "
+    @@g_f = Integer(gets) rescue nil
+  end
+
+  puts "How many meals of those meals are nut free?: "
+  @@n_f = Integer(gets) rescue nil
+
+  until @@n_f != nil do
+    puts "Invalid input, please retype the number of nut free meals been ordered: "
+    @@n_f = Integer(gets) rescue nil
+  end
+
+  puts "How many meals of those meals are fish free?: "
+  @@f_f = Integer(gets) rescue nil
+
+  until @@f_f != nil do
+    puts "Invalid input, please retype the number of fish free meals been ordered: "
+    @@f_f = Integer(gets) rescue nil
+  end
+
+  #Error checking if the total number of gluten, nut, vegetarian, fish > Total meals
+
+  order_team = Team_Order. new
+  order_team.initializer(@@t_c, @@veg, @@g_f, @@n_f, @@f_f)
+
+  # Algorithm, find which sub group is the biggest from the order
+  # Select the 1st restaurant base upon the closest amount that can handle the number of meals
+
+
+end
+
+
+
+Main
