b0b48cf690e9b6679da2beb8bf4aeeda4e63cd42
howaiyung413 <howaiyung1988@gmail.com>
2017-01-18 14:31:44 -0700

    Added sorting function to trests class

--- main.rb
+++ main.rb
@@ -81,15 +81,14 @@ class Meal_order
       input_check("Order meal", "fish_free", f_f)
 
       raise ArgumentError.new("The total amount of meals is less than the amount of specialized meals been ordered. Please retype your order again.") if (t_c<(veg+g_f+n_f+f_f))
-
-
-      @total_meals = t_c
-      @vegetarian = veg
-      @gluten_free = g_f
-      @nut_free = n_f
-      @fish_free = f_f
+      @total_meals, @vegetarian, @gluten_free, @nut_free, @fish_free = t_c, veg, g_f, n_f, f_f
 
   end
+
+  def inspect
+    "#@total_meals #@vegetarian #@gluten_free #@nut_free #@fish_free"
+  end
+
 end
 
 class Restaurant < Meal_order
@@ -132,41 +131,37 @@ class Restaurants
 
 
     if @rests.any?
+
+      rests.each do |rest|
+        input_check(rest.rest_name, "rest_rating", rest.rest_rating)
+        input_check(rest.rest_name, "vegetarian", rest.vegetarian)
+        input_check(rest.rest_name, "gluten_free", rest.gluten_free)
+        input_check(rest.rest_name, "nut_free", rest.nut_free)
+        input_check(rest.rest_name, "fish_free", rest.fish_free)
+      end
       rest = @rests.first
 
       if rest.respond_to? (sort_type)
-        #temp_rests = @rests.sort_by(&:rest_rating) if sort_type == 'rest_rating'
-
-        #puts temp_rests.first.rest_rating
-
+        temp_rests = @rests.sort_by { |a| a.rest_name } if sort_type == 'rest_name'
         temp_rests = @rests.sort_by { |a| a.rest_rating }.reverse if sort_type == 'rest_rating'
-
-
-        #temp_rests.sort_by {|rest|, rest.rest_rating} if sort_type == 'rest_rating'
+        temp_rests = @rests.sort_by { |a| a.total_meals }.reverse if sort_type == 'total_meals'
+        temp_rests = @rests.sort_by { |a| a.vegetarian }.reverse if sort_type == 'vegetarian'
+        temp_rests = @rests.sort_by { |a| a.gluten_free }.reverse if sort_type == 'gluten_free'
+        temp_rests = @rests.sort_by { |a| a.nut_free }.reverse if sort_type == 'nut_free'
+        temp_rests = @rests.sort_by { |a| a.fish_free }.reverse if sort_type == 'fish_free'
       else
         raise ArgumentError.new("Attribute does not exist for a restaurant")
       end
-      #if rest.has_attribute?( sort_type )
-      #  puts "It works!"
-      #end
+
     else
       raise ArgumentError.new("No restaurants entered into the database")
     end
-    #@rests.each do |rest|
-    #  puts rest
-    #end
-    #if rest.has_attribute?( sort_type )
-    #  puts "It works!"
-    #end
 
-
+    return temp_rests
   end
 
   def get_rest_order(team_order)
-    #Sort the list of restaurants by ratings
-    #Subtract from the top restaurant
-    # if any individuals are left, provide error message of not
-    # enable to fulfill the order
+
   end
 
 
@@ -197,9 +192,9 @@ end
 
 class Main
 
-  #puts "Welcome to my restaurant customizer"
+  puts "Welcome to my restaurant customizer"
 
-  #order_team = Meal_order.new_using_order_meals
+  order_team = Meal_order.new_using_order_meals
 
   #restaurants = Restaurants.new
 
@@ -218,8 +213,8 @@ class Main
   #restaurants.rests.push(Restaurant.new("Phillips",5,{total_meals: 40, vegetarian: 5, gluten_free: 2, nut_free: 2, fish_free: 2}))
   #restaurants.rests.push(Restaurant.new("Chicken LaFlay",4,{total_meals: 20, gluten_free: 1, nut_free: 1, fish_free: 1}))
 
-  restaurants.rests.push(Restaurant.new("Chicken",4,5,1,1,1,1))
-  restaurants.rests.push(Restaurant.new("Phillips",5,5,1,1,1,1))
+  #restaurants.rests.push(Restaurant.new("Chicken",4,5,1,1,1,1))
+  #restaurants.rests.push(Restaurant.new("Phillips",5,5,1,1,1,nil))
 
   restaurants.sort_by('rest_rating')
 
