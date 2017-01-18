ec10055a2f8250b1f1827f7e6d33cc682fbcd90c
howaiyung413 <howaiyung1988@gmail.com>
2017-01-17 18:41:07 -0700

    Add test cases for function initialize for Meal_order, refined error checking for input

--- main.rb
+++ main.rb
@@ -1,17 +1,115 @@
 #C:\Ruby22-x64\bin -w
 
 
-class Meal_order
-  attr_accessor :total_meals
+
+def input_check(hashname, objecthash, object)
+
+  object_string = object.to_s
+  #puts object_string
+
+  #if (object.is_a? Integer)
+
+  #puts object_temp
+
+  #if !(object_temp =~ /^[0-9A-F]+$/i)
+  #puts object_string
+
+  if (object.is_a? Integer) && !(object_string =~ /^[0-9][A-F]+$/i) && !(object.is_a? Float)
+    temp_num = Integer(object).to_i rescue nil
+  else
+    raise ArgumentError.new("Improper input for " + hashname + ", attribute "+ objecthash)
+  end
+
+  if temp_num == nil || temp_num<0
+    raise ArgumentError.new("Improper input for " + hashname + ", attribute "+ objecthash)
+  end
+  return temp_num
+
+
 end
 
-class Restaurant
-  attr_accessor :rest_name, :rest_rating, :meals_served
+class Meal_order
+  attr_accessor :total_meals, :vegetarian, :gluten_free, :nut_free, :fish_free
 
-  def initialize(name, rating, attributes)
+
+
+  def self.get_info(first_msg, error_msg)
+    temp_num = 0
+
+    print first_msg
+    temp_num = Integer(gets).to_i rescue nil
+
+    until temp_num != nil and temp_num >= 0
+      print error_msg
+      temp_num = Integer(gets) rescue nil
+    end
+
+    return temp_num
+  end
+
+  def self.new_using_order_meals()
+    t_c=0
+    veg=0
+    g_f=0
+    n_f=0
+    f_f=0
+
+    begin
+
+      t_c = get_info("How many meals are we ordering for the team?: ", "Invalid input, please retype the number of meals been ordered: ")
+      veg = get_info("How many meals of those meals are vegetarian?: ", "Invalid input, please retype the number of vegetarian meals been ordered: ")
+      g_f = get_info("How many meals of those meals are gluten free?: ", "Invalid input, please retype the number of gluten free meals been ordered: ")
+      n_f = get_info("How many meals of those meals are nut free?: ", "Invalid input, please retype the number of nut free meals been ordered: ")
+      f_f = get_info("How many meals of those meals are fish free?: ", "Invalid input, please retype the number of fish free meals been ordered: ")
+
+
+      puts "The total amount of meals is less than the amount of specialized meals been ordered. Please retype your order again." if (t_c<(veg+g_f+n_f+f_f))
+
+    end until ((veg+g_f+n_f+f_f) <= t_c)
+
+    self.new(t_c, veg, g_f, n_f, f_f)
+
+  end
+
+
+
+  def initialize(t_c, veg, g_f, n_f, f_f)
+
+      input_check("Order meal", "Total_meals", t_c)
+      input_check("Order meal", "vegetarian", veg)
+      input_check("Order meal", "gluten_free", g_f)
+      input_check("Order meal", "nut_free", n_f)
+      input_check("Order meal", "fish_free", f_f)
+
+      raise ArgumentError.new("The total amount of meals is less than the amount of specialized meals been ordered. Please retype your order again.") if (t_c<(veg+g_f+n_f+f_f))
+
+
+      @total_meals = t_c
+      @vegetarian = veg
+      @gluten_free = g_f
+      @nut_free = n_f
+      @fish_free = f_f
+
+  end
+end
+
+class Restaurant < Meal_order
+  attr_accessor :rest_name, :rest_rating
+
+
+
+
+  def initialize(name, rating, t_c, veg, g_f, n_f, f_f)
+
+    input_check(name, "Total_meals", t_c)
+    input_check(name, "vegetarian", veg)
+    input_check(name, "gluten_free", g_f)
+    input_check(name, "nut_free", n_f)
+    input_check(name, "fish_free", f_f)
+
+    super(t_c, veg, g_f, n_f, f_f)
     @rest_name=name
     @rest_rating=rating
-    @meals_served=attributes
   end
 
 end
@@ -26,16 +124,18 @@ class Restaurants
   end
 
   def sort_by(sort_type)
+
     for rest in @rests
-      if rest.meals_served.has_attribute?(sort_type)
-        puts "True"
-      end
+
+      #puts rest.meals_served.key?(:vegetarian)
+      #puts rest.meals_served.key?(sort_type)
+      #puts rest.key?(sort_type)
     end
 
   end
 
 
-# Delete restaurants that don't have specific meals that is required from the 
+# Delete restaurants that don't have specific meals that is required from the
 
 
 end
@@ -45,81 +145,40 @@ end
 
 
 
-def get_info(first_msg, error_msg)
-  temp_num = 0
-
-  print first_msg
-  temp_num = Integer(gets).to_i rescue nil
-
-  until temp_num != nil and temp_num > 0
-    print error_msg
-    temp_num = Integer(gets) rescue nil
-  end
-
-  return temp_num
-
-end
-
-def get_meals()
-  t_c=0
-  veg=0
-  g_f=0
-  n_f=0
-  f_f=0
-
-  begin
-    t_c = get_info("How many meals are we ordering for the team?: ", "Invalid input, please retype the number of meals been ordered: ")
-    veg = get_info("How many meals of those meals are vegetarian?: ", "Invalid input, please retype the number of vegetarian meals been ordered: ")
-    g_f = get_info("How many meals of those meals are gluten free?: ", "Invalid input, please retype the number of gluten free meals been ordered: ")
-    n_f = get_info("How many meals of those meals are nut free?: ", "Invalid input, please retype the number of nut free meals been ordered: ")
-    f_f = get_info("How many meals of those meals are fish free?: ", "Invalid input, please retype the number of fish free meals been ordered: ")
 
 
-    puts "The total amount of meals is less than the amount of specialized meals been ordered. Please retype your order again." if (t_c<(veg+g_f+n_f+f_f))
-
-  end until ((veg+g_f+n_f+f_f) <= t_c)
-
-  order_team = Meal_order.new
-  order_team.total_meals = t_c
-
-  if veg > 0
-    order_team.class.module_eval {attr_accessor :vegetarian}
-    order_team.vegetarian = veg
-  end
-
-  if g_f > 0
-    order_team.class.module_eval {attr_accessor :gluten_free}
-    order_team.gluten_free = g_f
-  end
-
-  if n_f > 0
-    order_team.class.module_eval {attr_accessor :nut_free}
-    order_team.nut_free = n_f
-  end
-
-  if f_f > 0
-    order_team.class.module_eval {attr_accessor :fish_free}
-    order_team.fish_free = f_f
-  end
-
-  return order_team
-end
 
 
 
 
 class Main
 
-  puts "Welcome to my restaurant customizer"
+  #puts "Welcome to my restaurant customizer"
 
-  order_team = get_meals()
+  #order_team = Meal_order.new_using_order_meals
 
   restaurants = Restaurants.new
 
-  restaurants.rests.push(Restaurant.new("Phillips",5,{total_meals: 40, vegetarian: 5, gluten_free: 2, nut_free: 2, fish_free: 2}))
-  restaurants.rests.push(Restaurant.new("Chicken LaFlay",4,{total_meals: 20, gluten_free: 1, nut_free: 1, fish_free: 1}))
+  #restaurants.rests.push(Restaurant.new("Phillips",5,2.5,0.1,0.1,0.1,0.1))
 
-  restaurants.sort_by("vegetarian")
+  #restaurants.rests.push(Restaurant.new("Phillips",5,nil,nil,nil,nil,nil))
+
+  restaurants.rests.push(Restaurant.new("Phillips",5,0x90,0x26,0x10,0x10,0x10))
+
+
+
+
+
+  #restaurants = Restaurants.new
+
+  #restaurants.rests.push(Restaurant.new("Phillips",5,{total_meals: 40, vegetarian: 5, gluten_free: 2, nut_free: 2, fish_free: 2}))
+  #restaurants.rests.push(Restaurant.new("Chicken LaFlay",4,{total_meals: 20, gluten_free: 1, nut_free: 1, fish_free: 1}))
+
+
+  #restaurants.sort_by(:vegetarian)
+
+  #restaurants.sort_by(:vegetarian)
+
   #Error checking if the total number of gluten, nut, vegetarian, fish > Total meals
 
 
---
+++ test_Meal_order.rb
@@ -0,0 +1,87 @@
+# File: main.rb
+
+require_relative "main"
+require "test/unit"
+
+class Test_Meal_order_ordinary_order < Test::Unit::TestCase
+
+<<-DOC
+  def setup
+    order_meals = Meal_order.new(20, 2, 2, 2, 2)
+  end
+
+  def test_get_info_string_messages
+    assert_nothing_raised( RuntimeError ){Meal_order.get_info("This is a good sign", "This is a bad sign") }
+  end
+
+  def test_get_info_char_messages
+    assert_nothing_raised( RuntimeError ){Meal_order.get_info('c', 'v') }
+  end
+DOC
+
+
+
+  def test_Meal_order_initializer_normal
+    assert_nothing_raised( RuntimeError ){Meal_order.new(20,2,2,2,2)}
+  end
+
+  def test_Meal_order_initializer_total_meals_smaller
+    assert_raise( ArgumentError ){Meal_order.new(5,2,2,2,2)}
+  end
+
+  def test_Meal_order_initializer_missing_var
+    assert_raise( ArgumentError ){Meal_order.new(20,2,2,2)}
+  end
+
+  def test_Meal_order_initializer_all_nil
+    assert_raise( ArgumentError ){Meal_order.new(nil,nil,nil,nil,nil)}
+  end
+
+  def test_Meal_order_initializer_all_floats
+    assert_raise( ArgumentError ){Meal_order.new(2.5,0.1,0.1,0.1,0.1)}
+  end
+
+  def test_Meal_order_initializer_all_float_chars
+    assert_raise( ArgumentError ){Meal_order.new('2.5','0.1','0.1','0.1','0.1')}
+  end
+
+  def test_Meal_order_initializer_all_int_chars
+    assert_raise( ArgumentError ){Meal_order.new('10','1','1','1','2')}
+  end
+
+  def test_Meal_order_initializer_all_chars
+    assert_raise( ArgumentError ){Meal_order.new('C','H','A','I','R')}
+  end
+
+  def test_Meal_order_initializer_all_strings
+    assert_raise( ArgumentError ){Meal_order.new("See","the","world","as","you")}
+  end
+
+  def test_Meal_order_initializer_all_equations_integers
+    assert_raise( ArgumentError ){Meal_order.new((4/2),((4*8)/2),(((9/3)*2)+3),((24*4)/6),(65/5))}
+  end
+
+  def test_Meal_order_initializer_all_equations_integers_strings
+    assert_raise( ArgumentError ){Meal_order.new("(4/2)","((4*8)/2)","(((9/3)*2)+3)","((24*4)/6)","(65/5)")}
+  end
+
+  # Can not stop the conversion from automatically from hex to an integer,
+  # a primitive conversion. Built into Ruby. 
+  def test_Meal_order_initializer_all_hex_integer
+    assert_raise( ArgumentError ){Meal_order.new(0x90,0x26,0x10,0x10,0x10)}
+  end
+
+  def test_Meal_order_initializer_all_hex_nils
+    assert_raise( ArgumentError ){Meal_order.new(0xf0a1,0xf0a1,0xf0a1,0xf0a1,0xf0a1)}
+  end
+
+  def test_Meal_order_initializer_max_value
+    n_BYTES = [100].pack('i').size
+    n_BITS = n_BYTES * 16
+    max = 2 ** (n_BITS - 2) - 1
+    mIN = -max - 1
+    assert_nothing_raised( ArgumentError ){Meal_order.new(max,1,1,1,1)}
+  end
+  #def test_Meal_order_initializer_
+
+end
