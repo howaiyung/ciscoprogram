2677dc08e20d17dff3f135772badbf10831043fe
howaiyung413 <howaiyung1988@gmail.com>
2017-01-19 03:21:12 -0700

    Finish creating order function

--- main.rb
+++ main.rb
@@ -7,6 +7,14 @@ def input_check(hashname, objecthash, object)
   object_string = object.to_s
 
   if object_string =~ /^[0-9]*$/ && !(object.nil?) #&& !(object_string.empty?)
+    if objecthash == "rest_rating"
+      if object.to_i <= 5
+        temp_num = object.to_i
+      else
+        raise ArgumentError.new("Improper input for " + hashname + ", attribute "+ objecthash)
+      end
+    end
+
     temp_num = object.to_i
   else
     raise ArgumentError.new("Improper input for " + hashname + ", attribute "+ objecthash)
@@ -17,6 +25,20 @@ def input_check(hashname, objecthash, object)
 
 
 end
+
+
+def check_meal(name, t_c, veg, g_f, n_f, f_f)
+  input_check("Order meal", "Total_meals", t_c)
+  input_check("Order meal", "vegetarian", veg)
+  input_check("Order meal", "gluten_free", g_f)
+  input_check("Order meal", "nut_free", n_f)
+  input_check("Order meal", "fish_free", f_f)
+
+  raise ArgumentError.new("The total amount of meals is less than the amount of specialized meals been ordered. Please retype your order again.") if (t_c<(veg+g_f+n_f+f_f))
+end
+
+
+
 
 class Meal_order
   attr_accessor :total_meals, :vegetarian, :gluten_free, :nut_free, :fish_free
@@ -74,13 +96,8 @@ class Meal_order
 
   def initialize(t_c, veg, g_f, n_f, f_f)
 
-      input_check("Order meal", "Total_meals", t_c)
-      input_check("Order meal", "vegetarian", veg)
-      input_check("Order meal", "gluten_free", g_f)
-      input_check("Order meal", "nut_free", n_f)
-      input_check("Order meal", "fish_free", f_f)
+      check_meal("Order meal", t_c, veg, g_f, n_f, f_f)
 
-      raise ArgumentError.new("The total amount of meals is less than the amount of specialized meals been ordered. Please retype your order again.") if (t_c<(veg+g_f+n_f+f_f))
       @total_meals, @vegetarian, @gluten_free, @nut_free, @fish_free = t_c, veg, g_f, n_f, f_f
 
   end
@@ -96,11 +113,7 @@ class Restaurant < Meal_order
 
   def initialize(name, rating, t_c, veg, g_f, n_f, f_f)
 
-    input_check(name, "Total_meals", t_c)
-    input_check(name, "vegetarian", veg)
-    input_check(name, "gluten_free", g_f)
-    input_check(name, "nut_free", n_f)
-    input_check(name, "fish_free", f_f)
+    check_meal(name, t_c, veg, g_f, n_f, f_f)
 
     super(t_c, veg, g_f, n_f, f_f)
     @rest_name=name
@@ -114,45 +127,47 @@ class Restaurants
 
   attr_accessor :rests
 
-
-
   def sort_by(sort_type)
-
-
-
-
-    #rest = @rests.at(0)
-
-    #puts rest
-
-    #temp_rest = rest.new(5,1,1,1,1)
 
     temp_rests = 0
 
 
-    if @rests.any?
+    if @rests.any?  # Check if there is any entries in the restaurants array
+      rests.each do |rest| # Go through each entry in the array
+        if rest.instance_of? Restaurant #Check if the resturant is an instance of Restaurant
+          # Check if each value in the
+          input_check(rest.rest_name, "rest_rating", rest.rest_rating)
+          check_meal(rest.rest_name, rest.total_meals, rest.vegetarian, rest.gluten_free, rest.nut_free, rest.fish_free)
 
-      rests.each do |rest|
-        input_check(rest.rest_name, "rest_rating", rest.rest_rating)
-        input_check(rest.rest_name, "vegetarian", rest.vegetarian)
-        input_check(rest.rest_name, "gluten_free", rest.gluten_free)
-        input_check(rest.rest_name, "nut_free", rest.nut_free)
-        input_check(rest.rest_name, "fish_free", rest.fish_free)
+        else
+          raise ArgumentError.new("Invalid restaurant entry")
+        end
+
       end
       rest = @rests.first
 
       if rest.respond_to? (sort_type)
-        temp_rests = @rests.sort_by { |a| a.rest_name } if sort_type == 'rest_name'
-        temp_rests = @rests.sort_by { |a| a.rest_rating }.reverse if sort_type == 'rest_rating'
-        temp_rests = @rests.sort_by { |a| a.total_meals }.reverse if sort_type == 'total_meals'
-        temp_rests = @rests.sort_by { |a| a.vegetarian }.reverse if sort_type == 'vegetarian'
-        temp_rests = @rests.sort_by { |a| a.gluten_free }.reverse if sort_type == 'gluten_free'
-        temp_rests = @rests.sort_by { |a| a.nut_free }.reverse if sort_type == 'nut_free'
-        temp_rests = @rests.sort_by { |a| a.fish_free }.reverse if sort_type == 'fish_free'
+        case sort_type
+        when 'rest_name'
+          temp_rests = @rests.sort_by { |a| a.rest_name}
+        when 'rest_rating'
+          temp_rests = @rests.sort_by { |a| [-a.rest_rating, a.rest_name] }
+        when 'total_meals'
+          temp_rests = @rests.sort_by { |a| [-a.total_meals, a.rest_name] }
+        when 'vegetarian'
+          temp_rests = @rests.sort_by { |a| [-a.vegetarian, a.rest_name] }
+        when 'gluten_free'
+          temp_rests = @rests.sort_by { |a| [-a.gluten_free, a.rest_name] }
+        when 'nut_free'
+          temp_rests = @rests.sort_by { |a| [-a.nut_free, a.rest_name] }
+        when 'fish_free'
+          temp_rests = @rests.sort_by { |a| [-a.fish_free, a.rest_name] }
+        else
+          raise ArgumentError.new("Attribute does not exist for a restaurant")
+        end
       else
         raise ArgumentError.new("Attribute does not exist for a restaurant")
       end
-
     else
       raise ArgumentError.new("No restaurants entered into the database")
     end
@@ -160,8 +175,73 @@ class Restaurants
     return temp_rests
   end
 
+  def get_part_meal_order(meal_attr, meal_type, rest_order, team_order, rest)
+
+    if(team_order.send(meal_attr) -  rest.send(meal_attr) < 0)
+      rest.total_meals -= team_order.send(meal_attr)
+      rest_order.push(team_order.send(meal_attr))
+      team_order.total_meals -= team_order.send(meal_attr)
+      team_order.send(meal_type, 0)
+    else
+      rest_order.push(rest.send(meal_attr))
+      team_order.send(meal_type, team_order.send(meal_attr)-rest.send(meal_attr))
+      rest.total_meals -= rest.send(meal_attr)
+      team_order.total_meals -= rest.send(meal_attr)
+    end
+  end
+
   def get_rest_order(team_order)
 
+    temp_rest_meals = Array.new
+
+
+    rests_sort = sort_by('rest_rating')
+
+    check_meal("Order meal", team_order.total_meals, team_order.vegetarian, team_order.gluten_free, team_order.nut_free, team_order.fish_free)
+
+    rests_sort.each do |rest|
+    #  temp_rest_meals = rest.instance_variables.map{|var| print rest.instance_variable_get(var)}
+      temp_order_rest = Array.new
+
+      temp_order_rest.push(rest.rest_name)
+
+      get_part_meal_order(:vegetarian, "vegetarian=", temp_order_rest, team_order, rest)
+      if team_order.total_meals == 0
+        temp_rest_meals.push(temp_order_rest)
+        break
+      end
+
+      get_part_meal_order(:gluten_free, "gluten_free=", temp_order_rest, team_order, rest)
+      if team_order.total_meals == 0
+        temp_rest_meals.push(temp_order_rest)
+        break
+      end
+
+      get_part_meal_order(:nut_free, "nut_free=", temp_order_rest, team_order, rest)
+      if team_order.total_meals == 0
+        temp_rest_meals.push(temp_order_rest)
+        break
+      end
+
+      get_part_meal_order(:fish_free, "fish_free=", temp_order_rest, team_order, rest)
+      if team_order.total_meals == 0
+        temp_rest_meals.push(temp_order_rest)
+        break
+      end
+
+      get_part_meal_order(:total_meals, "total_meals=", temp_order_rest, team_order, rest)
+      if team_order.total_meals == 0
+        temp_rest_meals.push(temp_order_rest)
+        break
+      end
+
+      temp_rest_meals.push(temp_order_rest)
+
+      puts team_order.inspect
+
+    end
+
+    puts "#{temp_rest_meals}"
   end
 
 
@@ -192,9 +272,9 @@ end
 
 class Main
 
-  puts "Welcome to my restaurant customizer"
+  #puts "Welcome to my restaurant customizer"
 
-  order_team = Meal_order.new_using_order_meals
+  #order_team = Meal_order.new_using_order_meals
 
   #restaurants = Restaurants.new
 
@@ -213,10 +293,10 @@ class Main
   #restaurants.rests.push(Restaurant.new("Phillips",5,{total_meals: 40, vegetarian: 5, gluten_free: 2, nut_free: 2, fish_free: 2}))
   #restaurants.rests.push(Restaurant.new("Chicken LaFlay",4,{total_meals: 20, gluten_free: 1, nut_free: 1, fish_free: 1}))
 
-  #restaurants.rests.push(Restaurant.new("Chicken",4,5,1,1,1,1))
-  #restaurants.rests.push(Restaurant.new("Phillips",5,5,1,1,1,nil))
+  restaurants.rests.push(Restaurant.new("Chicken",4,5,1,1,1,1))
+  restaurants.rests.push(Restaurant.new("Phillips",5,9,1,1,1,2))
 
-  restaurants.sort_by('rest_rating')
+  restaurants.get_rest_order(Meal_order.new(11,1,1,1,1))
 
   #restaurants.sort_by(:vegetarian)
 
